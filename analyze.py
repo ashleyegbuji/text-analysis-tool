6939a6a134f0802af4e62ab5f2e83d066ee33b21
Ashley Egbuji <nkemegbuji@gmail.com>
2025-04-28 11:48:18 +0100

    Obtained username from terminal input

---
+++ analyze.py
@@ -0,0 +1,9 @@
+print("\nWelcome to the text analysis tool, I will mine and analyze a body of text from a file you gave me!")
+
+# Get Username
+
+# Print message prompting user to input their name
+# Get input from user into the terminal
+username = input("\nTo begin, please enter your username:\n")
+# Greet the user
+print("Hello, " + username)
