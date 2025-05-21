8c38830d0135694a182d6f00683190c4344d1519
Ashley Egbuji <nkemegbuji@gmail.com>
2025-05-21 13:55:11 +0100

    Added part of speech to lemmatizer

--- analyze.py
+++ analyze.py
@@ -3,97 +3,114 @@ import nltk
 nltk.download('punkt_tab')
 from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk.stem import WordNetLemmatizer
+from nltk.corpus import wordnet
 nltk.download('wordnet')
+nltk.download('averaged_perceptron_tagger')
 wordLemmatizer = WordNetLemmatizer()
 import re
 
 # Welcome User
 def welcomeUser():
-    print("\nWelcome to the text analysis tool, I will mine and analyze a body of text from a file you gave me!")
+	print("\nWelcome to the text analysis tool, I will mine and analyze a body of text from a file you give me!")
 
 # Get Username
 def getUsername():
 
-    maxAttempts = 3
-    attempts = 0
+	maxAttempts = 3
+	attempts = 0
 
-  
-    while attempts < maxAttempts:
+	while attempts < maxAttempts:
 
-      # Print message prompting user to input their name
-      inputPrompt = ""
-      if attempts == 0:
-          inputPrompt = "\nTo begin, please enter your username:\n"
-   
-      else:  
-        inputPrompt = "\nPlease try again:\n"
-      usernameFromInput = input(inputPrompt)
+		# Print message prompting user to input their name
+		inputPrompt = ""
+		if attempts == 0:
+			inputPrompt = "\nTo begin, please enter your username:\n"
+		else:
+			inputPrompt = "\nPlease try again:\n"
+		usernameFromInput = input(inputPrompt)
 
-      # Validate Username
-      if len(usernameFromInput) < 5 or not usernameFromInput.isidentifier():
-          print("Username must be at least 5 characters long, alphanumeric only (a-z/A-Z/0-9), and underscore only, no spaces and cannot start with a number!")
-      else:    
-          return usernameFromInput
+		# Validate username
+		if len(usernameFromInput) < 5 or not usernameFromInput.isidentifier():
+			print("Your username must be at least 5 characters long, alphanumeric only (a-z/A-Z/0-9), have no spaces, and cannot start with a number!")
+		else:
+			return usernameFromInput
 
-      attempts =+ 1
-      
-    print("\nExhausted all " + str(maxAttempts) + " attempts, assigning username instead...")
-    return generate_username()[0]   
+		attempts += 1
 
+	print("\nExhausted all " + str(maxAttempts) + " attempts, assigning username instead...")
+	return generate_username()[0]
 
 # Greet the user
 def greetUser(name):
-    print("Hello, " + name)
+	print("Hello, " + name)
 
 # Get text from file
 def getArticleText():
-    f = open("files/article.txt", "r")
-    rawText = f.read()
-    f.close()
-    return rawText.replace("\n", " ").replace("\r", "") 
-    
-# Extract sentences    
+	f = open("files/article.txt", "r")
+	rawText = f.read()
+	f.close()
+	return rawText.replace("\n", " ").replace("\r", "")
+
+# Extract Sentences from raw Text Body
 def tokenizeSentences(rawText):
-    return sent_tokenize(rawText)    
+	return sent_tokenize(rawText)
 
 # Extract Words from list of Sentences
-def tokenizeWords(Sentences):
-    words = []
-    for sentence in sentences:
-        words.extend(word_tokenize(sentence))
-    return words
-def extractKeySentences(sentences):
-    matchedSentences = []
-    for sentence in sentences:
-        # If sentence matches desired pattern, add to matchedSentences
-        if re.search(searchPattern, sentence.lower()):
-           matchedSentences.append(sentence)
-    return matchedSentences       
+def tokenizeWords(sentences):
+	words = []
+	for sentence in sentences:
+		words.extend(word_tokenize(sentence))
+	return words
+
+# Get the key sentences based on search pattern of key words
+def extractKeySentences(sentences, searchPattern):
+	matchedSentences = []
+	for sentence in sentences:
+		# If sentence matches desired pattern, add to matchedSentences
+		if re.search(searchPattern, sentence.lower()):
+			matchedSentences.append(sentence)
+	return matchedSentences
 
 # Get the average words per sentence, excluding punctuation
 def getWordsPerSentence(sentences):
-    totalWords = 0
-    for sentence in sentences:
-        totalWords += len(sentence.split(" "))
-    return totalWords / numSentences
+	totalWords = 0
+	for sentence in sentences:
+		totalWords += len(sentence.split(" "))
+	return totalWords / len(sentences)
 
-# Filter raw tokenized words list to only include
-# valid english words
-def cleanseWordList(words):
+# Convert part of speech from pos_tag() function
+# into wordnet compatible pos tag
+posToWordnetTag = {
+	"J": wordnet.ADJ,
+	"V": wordnet.VERB,
+	"N": wordnet.NOUN,
+	"R": wordnet.ADV
+}
+def treebankPosToWordnetPos(partOfSpeech):
+	posFirstChar = partOfSpeech[0]
+	if posFirstChar in posToWordnetTag:
+		return posToWordnetTag[posFirstChar]
+	return wordnet.NOUN
+
+# Convert raw list of (word, POS) tuple to a list of strings
+# that only include valid english words
+def cleanseWordList(posTaggedWordTuples):
 	cleansedWords = []
 	invalidWordPattern = "[^a-zA-Z-+]"
-	for word in words:
+	for posTaggedWordTuple in posTaggedWordTuples:
+		word = posTaggedWordTuple[0]
+		pos = posTaggedWordTuple[1]
 		cleansedWord = word.replace(".", "").lower()
-		if (not re.search(invalidWordPattern, cleansedWord)) and len(word) > 1:
-			cleansedWords.append(wordLemmatizer.lemmatize(cleansedWord))
+		if (not re.search(invalidWordPattern, cleansedWord)) and len(cleansedWord) > 1:
+			cleansedWords.append(wordLemmatizer.lemmatize(cleansedWord, treebankPosToWordnetPos(pos)))
 	return cleansedWords
 
-# Get User details
+# Get User Details
 welcomeUser()
-username = getUsername()  
-greetUser(username)     
+username = getUsername()
+greetUser(username)
 
-# Extract and tokenize text 
+# Extract and Tokenize Text
 articleTextRaw = getArticleText()
 articleSentences = tokenizeSentences(articleTextRaw)
 articleWords = tokenizeWords(articleSentences)
@@ -104,7 +121,8 @@ keySentences = extractKeySentences(articleSentences, stockSearchPattern)
 wordsPerSentence = getWordsPerSentence(articleSentences)
 
 # Get Word Analytics
-articleWordsCleansed = cleanseWordList(articleWords)
+wordsPosTagged = nltk.pos_tag(articleWords)
+articleWordsCleansed = cleanseWordList(wordsPosTagged)
 
 # Print for testing
 print("GOT:")
