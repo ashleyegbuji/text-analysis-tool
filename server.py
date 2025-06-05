8d74b7299bb4062767aae2a6d8ab24bcc9f33bdb
Ashley Egbuji <nkemegbuji@gmail.com>
2025-06-05 01:12:00 +0100

    Integrated stockAnalyze code into analyze route handler

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 # Importing flask module in the project is mandatory
 # An object of Flask class is our WSGI application.
 from flask import Flask, abort
+from stockAnalyze import getCompanyStockInfo
+
 
 # Flask constructor takes the name of 
 # current module (__name__) as argument.
@@ -17,7 +19,8 @@ def healthCheck():
 def analyzeStock(ticker):
     if len(ticker) > 5 or not ticker.isidentifier():
         abort(400, 'Invalid ticker symbol')
-    return {data: 'Analysis for ' + ticker + ' coming soon'}
+    analysis = getCompanyStockInfo(ticker)
+    return analysis
 
 # main driver function
 if __name__ == '__main__':
